a322335295ce0d26153f4302053aa1ed75f945d5
rookxd <muzafferpro323232@gmail.com>
2024-12-21 07:20:23 +0300

    Update fxmanifest.lua

--- rook-redzone/fxmanifest.lua
+++ rook-redzone/fxmanifest.lua
@@ -1,13 +1,8 @@
-shared_script '@fiveguard/shared_fg-obfuscated.lua'
-shared_script '@ox-getkey/ai_module_fg-obfuscated.lua'
-shared_script '@ox-getkey/shared_fg-obfuscated.lua'
+
 
 
 
  
-lua54 'yes' -- needed for reaper
-lua54 'yes' -- needed for reaper
-shared_script 'atrigger.lua'
 fx_version 'cerulean'
 lua54 'yes'
 game 'gta5'
